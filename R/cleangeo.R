1876a1111e23110d9526ceda4a5736882b338999
eblondel <emmanuel.blondel1@gmail.com>
2015-09-25 01:50:05 +0200

    #5 provide vignette and add missing doc

---
+++ R/cleangeo.R
@@ -0,0 +1,19 @@
+#' A Set of Utility Tools to Clean Geometries
+#'
+#' cleangeo provides you a set of utility tools to inspect spatial objects,
+#' report errors by type and clean geometries.
+#'
+#' @docType package
+#' @name cleangeo
+#' @author Emmanuel Blondel \email{emmanuel.blondel1@@gmail.com}
+#' @details
+#' \tabular{ll}{
+#'    Package: \tab rsdmx\cr
+#'    Type: \tab Package\cr
+#'    Version: \tab 0.1\cr
+#'    Date: \tab 2015-09-25\cr
+#'    License: \tab GPL(>=2.0)\cr
+#'    LazyLoad: \tab yes\cr
+#'  }
+#' 
+NULL
