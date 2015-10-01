e7277d005960fd7dbd5c9ff80bf29b39393de966
eblondel <emmanuel.blondel1@gmail.com>
2015-10-01 15:01:56 +0200

    #8 fix vignette builder

--- R/cleangeo.R
+++ R/cleangeo.R
@@ -13,7 +13,7 @@
 #'    Package: \tab cleangeo\cr
 #'    Type: \tab Package\cr
 #'    Version: \tab 0.1-1\cr
-#'    Date: \tab 2015-09-28\cr
+#'    Date: \tab 2015-10-01\cr
 #'    License: \tab GPL(>=2.0)\cr
 #'    LazyLoad: \tab yes\cr
 #'  }
