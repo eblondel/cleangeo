d7a2ad0fd9ab4edaa1785d56b0bcdb3ad18847fa
eblondel <emmanuel.blondel1@gmail.com>
2016-02-08 11:09:21 +0100

    #9 add validation tests (needs visual check on plots)

--- tests/testthat/test_Clean.R
+++ tests/testthat/test_Clean.R
@@ -58,3 +58,67 @@ test_that("clgeo_Clean",{
   expect_true(all(report.fixed[,"valid"]))
   expect_equal(nv.fixed, NA)
 })
+
+#Clean cases
+#(list of cases taken from https://github.com/tudelft3d/prepair)
+
+#TODO case to investigate
+#-> validation using gBuffer NOT OK (lose of area)
+test_that("Clean - 'bowtie' polygons",{
+  wkt <- "POLYGON((0 0, 0 10, 10 0, 10 10, 0 0))"
+  sp <- rgeos::readWKT(wkt)
+})
+
+#validation is OK (managed by rgeos)
+test_that("Clean - Square with wrong orientation",{
+  wkt <- "POLYGON((0 0, 0 10, 10 10, 10 0, 0 0))"
+  sp <- rgeos::readWKT(wkt)
+  expect_true(gIsValid(sp))
+})
+
+#TODO case to investigate
+#-> validation may be considered OK (nested ring is removed)
+#-> validation may be considered NOT OK? (lose of area - expect 2 overlapping polygons)
+test_that("Clean - Inner ring with one edge sharing part of an edge of the outer ring",{
+  wkt <- "POLYGON((0 0, 10 0, 10 10, 0 10, 0 0),(5 2,5 7,10 7, 10 2, 5 2))"
+  sp <- rgeos::readWKT(wkt)
+  sp.clean <- clgeo_Clean(sp)
+  expect_false(gIsValid(sp))
+  expect_true(gIsValid(sp.clean))
+})
+
+#validation is OK (managed by cleangeo)
+test_that("Clean - Dangling edge",{
+  wkt <- "POLYGON((0 0, 10 0, 15 5, 10 0, 10 10, 0 10, 0 0))"
+  sp <- rgeos::readWKT(wkt)
+  sp.clean <- clgeo_Clean(sp)
+  expect_false(gIsValid(sp))
+  expect_true(gIsValid(sp.clean))
+})
+
+#TODO case to investigate
+test_that("Clean - Outer ring not closed",{
+  wkt <- "POLYGON((0 0, 10 0, 10 10, 0 10))"
+  #sp <- rgeos::readWKT(wkt)
+  #sp.clean <- clgeo_Clean(sp)
+  #expect_false(gIsValid(sp))
+  #expect_true(gIsValid(sp.clean))
+})
+
+#validation is OK (managed by cleangeo)
+test_that("Clean - Two adjacent inner rings",{
+  wkt <- "POLYGON((0 0, 10 0, 10 10, 0 10, 0 0), (1 1, 1 8, 3 8, 3 1, 1 1), (3 1, 3 8, 5 8, 5 1, 3 1))"
+  sp <- rgeos::readWKT(wkt)
+  sp.clean <- clgeo_Clean(sp)
+  expect_false(gIsValid(sp))
+  expect_true(gIsValid(sp.clean))
+})
+
+#TODO case to investigate
+test_that("Clean - Polygon with an inner ring inside another inner ring:",{
+  wkt <- "POLYGON((0 0, 10 0, 10 10, 0 10, 0 0), (2 8, 5 8, 5 2, 2 2, 2 8), (3 3, 4 3, 3 4, 3 3))"
+  sp <- rgeos::readWKT(wkt)
+  sp.clean <- clgeo_Clean(sp)
+  expect_false(gIsValid(sp))
+  expect_true(gIsValid(sp.clean))
+})
