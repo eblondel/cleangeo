c34cd70f33667f95bf87d811bbd06519f6aa308a
eblondel <emmanuel.blondel1@gmail.com>
2016-04-18 10:41:10 +0200

    version 0.2

--- R/cleangeo.R
+++ R/cleangeo.R
@@ -12,7 +12,7 @@
 #' \tabular{ll}{
 #'    Package: \tab cleangeo\cr
 #'    Type: \tab Package\cr
-#'    Version: \tab 0.1-2\cr
+#'    Version: \tab 0.2\cr
 #'    Date: \tab 2016-04-18\cr
 #'    License: \tab GPL(>=2.0)\cr
 #'    LazyLoad: \tab yes\cr
