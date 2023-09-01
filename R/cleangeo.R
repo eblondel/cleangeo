7008ad5e3aaf55615a80aef96a75d00545785a35
eblondel <emmanuel.blondel1@gmail.com>
2023-09-01 10:56:08 +0200

    fix Rdoc - :rocket: :package: on CRAN

--- R/cleangeo.R
+++ R/cleangeo.R
@@ -1,23 +1,17 @@
-#' Clean Geometries from Spatial Objects
+#' @name cleangeo
+#' @aliases cleangeo-package
+#' @aliases cleangeo
+#' @docType package
+#' 
+#' @title Clean Geometries from Spatial Objects
 #'
-#' cleangeo provides a set of utility tools to inspect spatial objects, facilitate handling and 
+#' @description cleangeo provides a set of utility tools to inspect spatial objects, facilitate handling and 
 #' reporting of topology errors and geometry validity issues.  Finally, it provides a geometry 
 #' cleaner that will fix all geometry problems, and  eliminate (at least reduce) the likelihood 
 #' of having issues when doing spatial data processing.
 #'
 #' @import sf
-#'
-#' @docType package
-#' @name cleangeo
+
 #' @author Emmanuel Blondel \email{emmanuel.blondel1@@gmail.com}
-#' @details
-#' \tabular{ll}{
-#'    Package: \tab cleangeo\cr
-#'    Type: \tab Package\cr
-#'    Version: \tab 0.2-4\cr
-#'    Date: \tab 2021-04-17\cr
-#'    License: \tab GPL(>=2.0)\cr
-#'    LazyLoad: \tab yes\cr
-#'  }
 #' 
 NULL
